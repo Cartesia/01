bb7fbb6b860bec7bf17f6c294de35c2a82c3771e
OaSiis <maxcharbonnelle@gmail.com>
2016-02-04 15:56:08 +0100

    merge

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,50 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $slug = $request->get('slug_brand');
+
+        if(!$slug){
+            $brand = $em->getRepository('AppBundle:Brand')->getFirstBrand();
+            if(!$brand){
+                throw new NotFoundHttpException("404");
+            }
+            return $this->redirectToRoute('show_template', ['slug_brand' => $brand[0]->getSlug()], 301);
+        }else{
+            $brand = $em->getRepository('AppBundle:Brand')->findOneBySlug($slug);
+            if(!$brand){
+                throw new NotFoundHttpException("404");
+            }
+        }
+
+        $alternatives = $em->getRepository('AppBundle:Alternative')->findAll();
+
+
+        $templates = $em->getRepository('AppBundle:Template')->getTemplatesByBrand($brand);
+
+        $arrayTemplates = [];
+
+        foreach($templates as $template){
+            $listBlocs = $em->getRepository('AppBundle:Bloc')->getBlocsByTemplate($template);
+            $arrayTemplates[$template->getId()]['template'] = $template ;
+            $arrayTemplates[$template->getId()]['blocs'] = $listBlocs ;
+        }
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('AppBundle:Default:index.html.twig', array(
+            'arrayTemplates' => $arrayTemplates,
+            'alternatives' => $alternatives,
+        ));
     }
 }
