4b64cce2fde405e7cd0aa4e51d485df7a2f4f696
Ankanitti <b.guillaume.iim@gmail.com>
2016-02-05 18:32:54 +0100

    started DnD jQuery UI

--- web/js/script.js
+++ web/js/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
     console.log('DOM+jQuery ready...');
 
-    $('.block-draggable').draggable();
+        $(".block-draggable").draggable({
+            cancel : false,
+            helper :'clone'
+        });
 
+        $(".block-drop-zone").droppable({
+            accept : ".block-draggable",
+            drop : function(event,ui){
+                console.log("Item was Dropped");
+                $(this).append($(ui.draggable).clone().data('html'));
+            }
+        });
 
 });
 
