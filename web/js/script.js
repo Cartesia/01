78edc3931326953561c6f983b6a2c2461776866d
Ankanitti <b.guillaume.iim@gmail.com>
2016-02-08 13:14:34 +0100

    numerotation update done

--- web/js/script.js
+++ web/js/script.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
 
+    // "remove-button" node
     var rmvBtn = "<button class='remove-button'>x</button>";
 
+    // function to update index on sortable
+    var updateIndex = function() {
+        var items = $('.sortable').find('li');
+        items.each(function(i) {
+            $(this).attr('data-order',i + 1);
+        });
+    };
 
+    // enable DnD of block elements
     $(".block-draggable").draggable({
         cancel : false,
         helper :'clone'
     });
 
+    // enable droppable zone
     $(".preview-zone__sortable").droppable({
         accept : ".block-draggable",
         drop : function(event,ui) {
@@ -19,15 +29,14 @@ $(document).ready(function() {
         }
     });
 
-    $( ".sortable" ).sortable()
-                    .disableSelection()
-                    .on("click", ".remove-button", function() {
-                        $(this).parent().remove();
-                    });
+    // endable sortable, update index on reorder
+    $( ".sortable" ).sortable({
+        stop: updateIndex
+    })
+        .disableSelection()
+        .on("click", ".remove-button", function() {
+            $(this).parent().remove();
+            updateIndex();
+        });
 
 });
-
-
-
-
-
