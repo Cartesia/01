c19c0d731f738682f4e3dc982ecf46b5584c259e
Ankanitti <b.guillaume.iim@gmail.com>
2016-02-08 09:12:56 +0100

    DnD/sortables

--- web/js/script.js
+++ web/js/script.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
     console.log('DOM+jQuery ready...');
 
-        $(".block-draggable").draggable({
-            cancel : false,
-            helper :'clone'
-        });
+    $(".block-draggable").draggable({
+        cancel : false,
+        helper :'clone'
+    });
 
-        $(".block-drop-zone").droppable({
-            accept : ".block-draggable",
-            drop : function(event,ui){
-                console.log("Item was Dropped");
-                $(this).append($(ui.draggable).clone().data('html'));
-            }
-        });
+    $(".preview-zone__sortable").droppable({
+        accept : ".block-draggable",
+        drop : function(event,ui){
+            console.log("Item was Dropped");
+            $(this).append($(ui.draggable).clone().data('html'));
+            var n = $('.preview-zone__sortable li').size();
+            $(this).find("li").last().attr('data-order',n);
+        }
+    });
+
+    $( ".sortable" ).sortable()
+                    .disableSelection();
+
+
 
 });
 
