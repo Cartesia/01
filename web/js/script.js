a913cde034a99b1b38e52751bb1b3ac35fb94785
OaSiis <maxcharbonnelle@gmail.com>
2016-02-09 16:18:43 +0100

    download

--- web/js/script.js
+++ web/js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 
 
 
+
     // function to update index on sortable
     function updateIndex() {
         var items = $('.sortable').find('li');
@@ -143,6 +144,23 @@ $(document).ready(function() {
 
     $(".preview-zone__sortable").on('click', 'a', function(e) {
         e.preventDefault();
+    });
+
+    // download
+    function downloadInnerHtml(filename, elId, mimeType) {
+        var elHtml = document.getElementById(elId).innerHTML;
+        var link = document.createElement('a');
+        mimeType = mimeType || 'text/plain';
+
+        link.setAttribute('download', filename);
+        link.setAttribute('href', 'data:' + mimeType  +  ';charset=utf-8,' + encodeURIComponent(elHtml));
+        link.click();
+    }
+
+    var fileName =  'tags.html'; // You can use the .txt extension if you want
+
+    $('#downloadLink').click(function(){
+        downloadInnerHtml(fileName, 'main','text/html');
     });
 
 });
